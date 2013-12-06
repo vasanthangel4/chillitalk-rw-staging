da74ea6d30ba9c8b43941e5f0a092dd56a91be6c
vasanth <vasanthangel4@gmail.com>
2013-12-07 00:52:19 +0530

    fdewfe

--- wp-content/themes/new_chillitalk/application/language/english/app_lang.php
+++ wp-content/themes/new_chillitalk/application/language/english/app_lang.php
@@ -1,96 +1,95 @@
 <?php
 
-$lang['app.first']= ascii_to_entities('<a name="id1"> <h1 class="maincomon">iPhone  </h1>
+
+
+$lang['app.first']= ascii_to_entities('<h1 class="maincomon"><a name="id1" style="color:#666666 !important;">iPhone   </a> </h1>
 <div class="maincomon texzx imgabb0"><p>
-Use your iPhone to make free calls to other Chillitalk App users and cheap international calls to everyone else. Make calls on the App, just like you would with your phone, and reduce the cost of your international calls. <b>Try it now.</b><br/><br/>
+Use your iPhone to make cheap international calls and FREE calls to other App users. Making calls on the App is just like making a usual call from your iPhone.  <br/><br/>
 </p>');
  
  
 $lang['app.second']= ascii_to_entities("<ul class='maincomon texzx'>
 
- <li><b> - Cheap international calls & texts</b> 
- <br/>
- Make huge savings on international calls. 
+ <li>•	FREE calls to others on Chillitalk – pay nothing! 
  </li>
- <li><b> - Make free calls to other Chillitalk App users</b> <br/>
-Don’t pay a dime when calling other Chillitalk App users.</li>
- <li><b>- Call using Wi-Fi, 3G or your local minutes</b> <br/>
-
- Make calls over a Wi-Fi or 3G connection. If you prefer, use your local minutes.
+ <li>•	Cheap international calls and texts at unbeatable prices!</li>
+ <li>•	Make big savings on international calls and texts to your friends and family abroad 
   </li>
- <li><b>- Perfect voice quality</b> <br/>
-Enjoy crystal-clear sound quality when making calls.
+ <li>•	Call using Wi-Fi/ 3G for even cheaper calls. Save money immediately!  
+ </li>
+  <li>•	Superb voice quality – Crystal clear calls bring you closer to friends and family
  </li>
  
- </ul>  </div> </a>");
+ </ul>   </div> ");
  
    $lang['app.download']= "download the App";
    
-    $lang['app.Android']= "<a name='id2'><h1 class='maincomon'  id='Android1'>Android Phones </h1>
+    $lang['app.Android']= "<a name='id2' style='color:#666666 !important;'><h1 class='maincomon'>Android Phones   </h1>
 	<div class='maincomon texzx imgabb'>
   <p>
- Use your Android device to make free calls to other Chillitalk App users and cheap international calls to everyone else. Make calls on the App, just like you would with your phone, and reduce the cost of your international calls.<br/> <br/> 
+Enjoy FREE calls to other Chillitalk App users through your Android device and make cheap international calls and texts to everyone else. Make calls on the App just as you would with your phone and reduce the cost of your international calls. <br/> <br/> 
+
+<a href='http://chillitalk.co.uk/en/sign_up'>Sign-up</a>  in 30 seconds and start adding your international contacts<br/><br/>
  </p>
+ 
+ 
+ 
  ";
 
 
  $lang['app.Androidtxts']= ascii_to_entities("<ul class='maincomon texzx '>
 
- <li><b>- Cheap international calls & texts</b> 
- <br/>
-Make huge savings on international calls and texts 
+ <li>•	FREE calls to others on Chillitalk – pay nothing!
+
  </li>
- <li><b>- Make free calls to other Chillitalk App users</b> <br/>
-Don’t pay a dime when calling other Chillitalk App users</li>
- <li><b>- Call using Wi-Fi, 3G or your local minutes</b> <br/>
-Make calls over a Wi-Fi or 3G connection. If you prefer, use your local minutes.
+ <li>•	Cheap international calls and texts at unbeatable prices!</b></li>
+ <li>•	Call using Wi-Fi, 3G, or your local minutes. Save money immediately!
+
   </li>
- <li><b>- Perfect voice quality</b> <br/>
-Enjoy crystal-clear sound quality when making calls
+ <li>•	Superb voice quality – Crystal clear calls bring you closer to friends and family
  </li>
  
  </ul>
-  </div>
-  </a>
+  </div></a>
   ");
-    $lang['app.Androidtxts1']= ascii_to_entities("<a name='id0'><h1 class='maincomon'  id='PC1'>PC </h1>
+    $lang['app.Androidtxts1']= ascii_to_entities("<a name='id0' style='color:#666666 !important;'><h1 class='maincomon'>PC </h1>
 	<div class='maincomon texzx imgabb1'>
-  <p>Use Chillitalk on your desktop PC or laptop to make cheap international calls and free calls to other Chillitalk App users. You can even use the PC App to send low-cost international texts. The App is free to download and easy to use.<br/><br/></p>");
+  <p><a href='http://chillitalk.co.uk/en/sign_up'>Download the FREE Chillitalk App</a> for your Windows desktop/laptop to make cheap international calls abroad and FREE calls to other Chillitalk App users. You can even use the PC App to send low-cost international texts. <br/><br/></p>");
  
 
 
 
     $lang['app.Androidtxts2']= ascii_to_entities("<ul class='maincomon texzx '>
 
- <li><b>- Cheap international calls & texts</b> 
- <br/> Make huge savings with Chillitalk's superb rates</li>
-  <li><b>- Free calls</b> 
- <br/> Call other Chillitalk App users for free</li>
-  <li><b>- Call from anywhere</b> 
- <br/> Connect over the internet from anywhere in the world<br/><br/></li>  </div>  </a>"
+ <li>•	FREE calls to others on Chillitalk through your PC – pay nothing!</li>
+  <li>•	Cheap international calls and texts at unbeatable prices!</b> 
+ <br/>•	Make huge savings with Chillitalk's superb international call rates</li>
+  <li>•	Connect over the internet, from anywhere in the world!</li> 
+   <li>•	Superb voice quality – Crystal clear calls bring you closer to friends and family</li>  </div>  </a>"
  
 );
  
  
-$lang['app.Androidtxts3'] =  ascii_to_entities("<a name='id3' style='color:#666666 !important;'><h1 class='maincomon' id='landline1'>Landline</h1>
+$lang['app.Androidtxts3'] =  ascii_to_entities("<a name='id3' style='color:#666666 !important;'> 
+<h1 class='maincomon'>Landline / Mobile Phone</h1>
 <div class='maincomon texzx imgabb2'>
-<p>Use any landline to make cheap international calls. For each number you save on Chillitalk, we will give you a Local Number to save as a contact. Simply call your contact on this Local Number and pay the same low rate that you would if you were calling from the same country. 
-<br/><br/>
-This feature is available in more than <a href='http://www2.chillitalk.com/en/chillitalk_countries'>50 countries</a>.<br/><br/></p>");
+<p>Enjoy cheap international calls from a landline or mobile. We give you a unique Local Number for every international number you save on Chillitalk. Use this Local Number to call your contact abroad. This feature is available for over 
+<a href='http://chillitalk.co.uk/en/chillitalk_countries'>50 countries</a>.<br/><br/>
+
+<a href='http://chillitalk.co.uk/en/sign_up'>Sign-up</a>  in 30 seconds and start adding your international contacts<br/><br/>
+</p>");
   
   
 $lang['app.Androidtxts4']=  ascii_to_entities("<ul class='maincomon texzx'>
 
- <li><b>- Cheap international calls</b> 
- <br/> Make calls at unbeatably low rates</li>
-  <li><b>- No need for Wi-Fi or a smartphone</b> 
- <br/>Just use your regular cellphone or landline using your Local Numbers</li>
-  <li><b>- Start saving immediately</b> 
- <br/>Sign-up in 30 seconds and start adding your international contacts
+ <li>•	Calls and texts at unbeatably low rates!</li>
+  <li>•	No need for Wi-Fi or a Smartphone. </li>
+ <li>•	Just use your regular mobile phone or landline with your Local Numbers.</li>
+ <li>•	Save money immediately!</b> </li>
+  <li><br></li>
+  </ul>
 
-</li>
-  
- 
- </div></a>
+ </div>
+ </a>
  ");
  
--- wp-content/themes/new_chillitalk/application/views/application/app.php
+++ wp-content/themes/new_chillitalk/application/views/application/app.php
@@ -1,55 +1,65 @@
 <div class="maincomon strtat">
 
+
+
 <h1 class="maincomon">Our App </h1>
 <h2 class="maincomon ctsd">How would you like to use Chillitalk?</h2>
 <div class="ctsd1">
 <div id="radios1">
     <a id="link">
-            	<input type="radio" id="tim1" value="landline" name="group1" class="{src:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile.jpg',checked:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile.jpg',hover:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile_select.jpg',disabled:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile.jpg' }"   title="landline" /><br />
+            	<input type="radio" id="tim1" value="landline" name="group1" class="{src:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile.jpg',checked:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile.jpg',hover:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile_select.jpg',disabled:'<?php echo get_template_directory_uri(); ?>/img/landlineandmobile.jpg' }"   title="landline" />
                 </a>
                 
-               <a id="link1">
+                 <a id="link1">
                  <input type="radio" id="tim3" onclick="toggle('hide');" value="android" name="group1" class="{src:'<?php echo get_template_directory_uri(); ?>/img/android.png',checked:'<?php echo get_template_directory_uri(); ?>/img/android.png',hover:'<?php echo get_template_directory_uri(); ?>/img/android_select.png',disabled:'<?php echo get_template_directory_uri(); ?>/img/android.png'}"  title="android" />
                    </a>
-                 <a id="link2">
+                    <a id="link2">
                   <input type="radio" id="tim2" onclick="toggle('hide');" value="iPhone & iPad" name="group1"  class="{src:'<?php echo get_template_directory_uri(); ?>/img/iphone2.png',checked:'<?php echo get_template_directory_uri(); ?>/img/iphone2.png',hover:'<?php echo get_template_directory_uri(); ?>/img/iphone_select.png',disabled:'<?php echo get_template_directory_uri(); ?>/img/iphone2.png'}"  title="iPhone" />
                </a>  
-                <a id="link3">
+                 <a id="link3">
                  <input type="radio" id="tim4" onclick="toggle('hide');" value="PC" name="group1" class="{src:'<?php echo get_template_directory_uri(); ?>/img/PC-diselect.png',checked:'<?php echo get_template_directory_uri(); ?>/img/PC-diselect.png',hover:'<?php echo get_template_directory_uri(); ?>/img/PC_select.png',disabled:'<?php echo get_template_directory_uri(); ?>/img/PC-diselect.png'}"   title="PC" />
                  </a>  
               </div>
 
 
 </div>
+ <div class="maincomon">
+   <?php echo lang('app.Androidtxts3'); ?>
+    <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/" 
+    title="download the App" class="tryus  leftcom">Sign up now! First call free</a>
+      <?php echo lang('app.Androidtxts4'); ?>
+   
+  
+ </div>
+  <div class="maincomon">
+ <?php echo lang('app.Android'); ?>
+ <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/"
+ title="download the App" class="tryus  leftcom"><?php echo lang('app.download'); ?></a>
+  <?php echo lang('app.Androidtxts'); ?>
+ </div>
+ 
 <?php echo lang('app.first'); ?> 
-   <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/" title="download the App" class="tryus12 leftcom"><!--<?php echo lang('app.download'); ?>--></a>
+
+   <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up#iphone" 
+ title="download the App" class="tryus  leftcom"><?php echo lang('app.download'); ?></a>
  
 <?php echo lang('app.second'); ?> 
  
  
  <br/><br/>
 </p>
- <div class="maincomon">
- <?php echo lang('app.Android'); ?>
- <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/" title="download the App" class="tryus  leftcom"><?php echo lang('app.download'); ?></a>
-  <?php echo lang('app.Androidtxts'); ?>
- </div>
+
   <div class="maincomon">
   <?php echo lang('app.Androidtxts1'); ?>
-   <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/" title="download the App" class="tryus12 leftcom"><!--<?php echo lang('app.download'); ?>--></a>
+   <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/" 
+   title="download the App" class="tryus12 leftcom"><!--<?php echo lang('app.download'); ?>--></a>
    
     <?php echo lang('app.Androidtxts2'); ?>
    
    
  </div>
 
-   <div class="maincomon">
-   <?php echo lang('app.Androidtxts3'); ?>
-    <a href="<?php echo base_url(); ?><?php echo $this->session->userdata('lang'); ?>/sign_up/" title="download the App" class="tryus  leftcom">try us out with a free call</a>
-      <?php echo lang('app.Androidtxts4'); ?>
-   
   
- </div>
  
  
  
@@ -60,28 +70,8 @@
       </div>
       <a class="toplink" style="display: block;"></a>
       
- <style type="text/css">
 
-
-#radios1 img:nth-last-child(1)
- {
-    margin-top: 0px;
-}
-#radios1 img:nth-last-child(3), #tim2 + img,#tim4 + img {
-    margin-top: 17px !important;
-}
-#tim1 + img {
-    margin-left: 0 !important;
-    margin-right: -8px !important;
-    margin-top: 3px;
-}
-
-#tim2 + img, #tim4 + img {
-    margin-top: 18px !important;
-}
-	  </style>
-	  
-	  <script type="text/javascript">
+<script type="text/javascript">
 	  
 	  function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
@@ -129,4 +119,8 @@ $("#link3").click(function() {
 });
 	
 </script> 
-
+<style>
+#radios1 {
+    margin: -14px 0 16px 100px;
+}
+</style>
