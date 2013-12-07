639f55cca662e71d9a732542c274848a45a375a7
Rori Kurniadi <rori.kurniadi@yahoo.com>
2013-12-07 16:26:37 +0530

    Update footer.php

--- wp-content/themes/new_chillitalk/application/views/template/footer.php
+++ wp-content/themes/new_chillitalk/application/views/template/footer.php
@@ -62,16 +62,7 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/javascript">
-var __lc = {};
-__lc.license = 3476332;
 
-(function() {
-	var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-	lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-})();
-</script>
 
 </body>
 </html>
