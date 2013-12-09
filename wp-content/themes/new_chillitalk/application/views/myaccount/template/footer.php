93b6f357aefa908dbd475ce120eaedc44b37647f
Rori Kurniadi <rori.kurniadi@yahoo.com>
2013-12-09 15:57:09 +0530

    Update footer.php

--- wp-content/themes/new_chillitalk/application/views/myaccount/template/footer.php
+++ wp-content/themes/new_chillitalk/application/views/myaccount/template/footer.php
@@ -60,6 +60,21 @@
 
 <?php wp_footer(); ?>
 
-
+<script type="text/javascript">
+    var Comm100API = Comm100API || new Object;
+    Comm100API.chat_buttons = Comm100API.chat_buttons || [];
+    var comm100_chatButton = new Object;
+    comm100_chatButton.code_plan = 13;
+    comm100_chatButton.div_id = 'comm100-button-13';
+    Comm100API.chat_buttons.push(comm100_chatButton);
+    Comm100API.site_id = 181012;
+    Comm100API.main_code_plan = 13;
+    var comm100_lc = document.createElement('script');
+    comm100_lc.type = 'text/javascript';
+    comm100_lc.async = true;
+    comm100_lc.src = 'https://chatserver.comm100.com/livechat.ashx?siteId=' + Comm100API.site_id;
+    var comm100_s = document.getElementsByTagName('script')[0];
+    comm100_s.parentNode.insertBefore(comm100_lc, comm100_s);
+</script>
 </body>
 </html>
